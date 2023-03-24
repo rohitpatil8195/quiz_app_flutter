08e29769667d28c7347dfb73c5e493048882ee7f
rohitpatil8195 <rohit.patil@einfochips.com>
2023-03-24 18:17:13 +0530

    shortened constructor code

--- lib/question.dart
+++ lib/question.dart
@@ -16,10 +16,7 @@
  class Question{
     String questionText;
     bool questionAnswer;
-   Question( String q, bool a){
-      questionText=q;
-       questionAnswer=a;
- }
+   Question( this.questionText, this.questionAnswer);
  }
 
  //Questions newQuestion=Questions();
